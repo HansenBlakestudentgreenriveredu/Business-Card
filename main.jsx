45469e736c3278b8c4ee45b9ae92d309ba90abbd
Blake Hansen <155667411+HansenBlakestudentgreenriveredu@users.noreply.github.com>
2025-10-19 14:02:32 -0700

    Add Main component with personal information

--- main.jsx
+++ main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './style.css';
 
+export default function Main() {
+    return (
+        <main className="Main">
+            <h1>Blake Hansen</h1>
+            <ul className="facts-list">
+                <li>Experienced Software Engineer</li>
+                <li>Expected Graduation 12/15/2025</li>
+            </ul>
+        </main>
+    )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
