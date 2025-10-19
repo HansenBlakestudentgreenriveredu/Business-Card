8a0aa0871f3c5f5c68f2c1b1244c36787953e1aa
Blake Hansen <155667411+HansenBlakestudentgreenriveredu@users.noreply.github.com>
2025-10-19 14:04:54 -0700

    Add Image component with profile photo and location

---
+++ components/Image.jsx
@@ -0,0 +1,10 @@
+export default function Image() {
+    return (
+        <header>
+            <nav>
+                <img src="/Picture.png" alt="Profile Photo" />
+                <span>Greater Seattle Area</span>
+            </nav>
+        </header>
+    );
+}
