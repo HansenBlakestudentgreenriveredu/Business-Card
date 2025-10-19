792f059b12edcdbe0a19b1a7f9f8925f057589f3
Blake Hansen <155667411+HansenBlakestudentgreenriveredu@users.noreply.github.com>
2025-10-19 14:02:57 -0700

    Add Main component with personal information

---
+++ components/Main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (
+        <main className="Main">
+            <h1>Blake Hansen</h1>
+            <ul className="facts-list">
+                <li>Experienced Software Engineer</li>
+                <li>Expected Graduation 12/15/2025</li>
+            </ul>
+        </main>
+    )
+}
