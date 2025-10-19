45beb0c99ae8c1e1b676421db9b09bab09a5d89c
Blake Hansen <155667411+HansenBlakestudentgreenriveredu@users.noreply.github.com>
2025-10-19 13:59:55 -0700

    Create App component with multiple subcomponents

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Image from "./components/image"
+import Main from "./components/Main"
+import Contact from "./components/Contact"
+import About from "./components/About"
+import Projects from "./components/Projects"
+
+export default function App() {
+    return (
+        <>
+            <Image />
+            <Main />
+            <Contact />
+            <About />
+            <Projects />
+        </>
+    )
+}
