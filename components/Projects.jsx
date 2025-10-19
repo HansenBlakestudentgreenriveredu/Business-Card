d93b1b0175725f45d3c090865627e6187e6a0dc4
Blake Hansen <155667411+HansenBlakestudentgreenriveredu@users.noreply.github.com>
2025-10-19 13:59:28 -0700

    Create Projects component with project listings

---
+++ components/Projects.jsx
@@ -0,0 +1,14 @@
+export default function Projects() {
+    return (
+        <main className="Projects">
+            <h2>Project 1</h2>
+            <p>Description</p>
+
+            <h2>Project 2</h2>
+            <p>Description</p>
+
+            <h2>Project 3</h2>
+            <p>Description</p>
+        </main>
+    )
+}
